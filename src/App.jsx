99ae08c9b98c06e1c80c4cab0f5c0d93a350e14e
Sushovan <sushovanghosh960@gmail.com>
2024-08-28 19:43:03 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import DateCounter from "./components/DateCounter";
+
+export default function App() {
+    return (
+        <>
+            <h1>A quizz App</h1>
+            <div>
+                <DateCounter />
+            </div>
+        </>
+    );
+};
