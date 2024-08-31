408559a6ccadc4ee7319c2aae07a49e7d2a8f9a7
Sushovan <sushovanghosh960@gmail.com>
2024-08-31 11:52:38 +0530

    create a json server
    implement a useReducer state to handle different states
    fetch data from json server and upload it to the state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,65 @@
-import DateCounter from "./components/DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+function reducer(state, action) {
+    switch (action.type) {
+        case "questionDataReceived":
+            return {
+                ...state,
+                questions: action.payload,
+                status: "ready"
+            };
+        case "failedToLoadData":
+            return {
+                ...state,
+                status: "error"
+            };
+        default:
+            throw new Error("unknown error occured!!")
+
+    }
+
+};
+
+const initialState = {
+    questions: [],
+
+    // loading, error, finished, ready, active
+    status: 'loading'
+};
 
 export default function App() {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(() => {
+        async function fetchQuestions() {
+            try {
+                const response = await fetch("http://localhost:8080/questions");
+                const data = await response.json();
+
+                dispatch({ type: "questionDataReceived", payload: data });
+            } catch (error) {
+                console.error(error);
+                dispatch({ type: "failedToLoadData" })
+            };
+        };
+
+        fetchQuestions();
+
+    }, []);
+
+    console.log(state);
+
+
+
     return (
-        <>
-            <h1>A quizz App</h1>
-            <div>
-                <DateCounter />
-            </div>
-        </>
+        <section className="app">
+            <Header />
+            <Main>
+                <p>1/15</p>
+                <p>Question</p>
+            </Main>
+        </section>
     );
 };
